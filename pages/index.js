2c920a89890eb28188e2571aa7c3be0bc70cb934
rahul@clearcut.design <rahul@clearcut.design>
2023-11-30 04:04:29 +0530

    basic chess board

--- pages/index.js
+++ pages/index.js
@@ -3,8 +3,10 @@ import React from "react";
 
 export default function Home() {
   return (
-    <div className="flex justify-center items-center h-screen bg-gray-100">
+    <div className="flex flex-col justify-center items-center h-screen bg-gray-100">
+      <h1 className="text-gray-600 text-3xl m-5"> BareBones Chess</h1>
       <Board />
+      <h1 className="text-gray-300 text-sm m-5">Made by Rahul Ragi</h1>
     </div>
   );
 }
