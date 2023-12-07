db048ca58bf34f2203d72755f906b4aaa3a0c4bf
rahul@clearcut.design <rahul@clearcut.design>
2023-12-07 17:03:30 +0530

    linting error

--- components/Square.js
+++ components/Square.js
@@ -4,8 +4,8 @@ import { motion } from "framer-motion";
 const Square = forwardRef(({ children, position, onClick }, ref) => {
   // Function to determine if the square is black or white
   const isBlackSquare = () => {
-    const file = position.charCodeAt(0) - "a".charCodeAt(0); // Convert file (letter) to number (a=0, b=1, c=2, ...)
-    const rank = parseInt(position[1], 10) - 1; // Convert rank to 0-indexed number
+    const file = position.charCodeAt(0) - "a".charCodeAt(0);
+    const rank = parseInt(position[1], 10) - 1;
     return (file + rank) % 2 === 1;
   };
 
@@ -27,5 +27,7 @@ const Square = forwardRef(({ children, position, onClick }, ref) => {
     </motion.div>
   );
 });
+
+Square.displayName = "Square"; // Set the display name
 
 export default Square;
