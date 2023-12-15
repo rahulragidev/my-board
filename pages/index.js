1a5f1577cca3e5bf83d85efbbc88fd453454ac67
rahul@clearcut.design <rahul@clearcut.design>
2023-12-16 05:10:48 +0530

    index page got changed

--- pages/index.js
+++ pages/index.js
@@ -1,53 +1,87 @@
-import React, { useState } from "react";
-import { motion } from "framer-motion";
+import React, { useState, useEffect } from "react";
+import { motion, AnimatePresence } from "framer-motion";
 import Board from "@/components/Board";
 import Image from "next/image";
 
 export default function Home() {
   const [isLogoAnimated, setIsLogoAnimated] = useState(false);
+  const [isContentVisible, setIsContentVisible] = useState(false);
+
+  useEffect(() => {
+    const image = new window.Image();
+    image.src = "/images/logo.png";
+    image.onload = () => console.log("Logo preloaded");
+
+    const prefersReducedMotion = window.matchMedia(
+      "(prefers-reduced-motion: reduce)"
+    ).matches;
+    if (prefersReducedMotion) {
+      setIsLogoAnimated(true);
+      setIsContentVisible(true);
+    }
+  }, []);
+
+  const handleAnimationComplete = () => {
+    setIsLogoAnimated(true);
+
+    // Delay the display of content for a smooth transition
+    setTimeout(() => {
+      setIsContentVisible(true);
+    }, 500); // Adjust the delay as needed for optimal user experience
+  };
+
+  const logoVariants = {
+    initial: { scale: 6, opacity: 0, rotate: 360 },
+    animate: { scale: 1, opacity: 1, rotate: 0 },
+    exit: { scale: 0.2, opacity: 0.5, top: 20, left: 20 },
+  };
+
+  const contentVariants = {
+    initial: { y: -30, opacity: 0 },
+    animate: { y: 0, opacity: 1 },
+  };
 
   return (
     <motion.div
-      initial={{ opacity: 0 }}
-      animate={{ opacity: 1 }}
-      transition={{ duration: 2 }} // Shortened duration for smoother transition
-      className="flex flex-col items-center justify-center min-h-screen bg-white text-gray-800"
+      initial="initial"
+      animate="animate"
+      exit="exit"
+      transition={{ duration: 2 }}
+      className="flex flex-col items-center justify-center min-h-screen bg-white text-gray-200"
     >
-      {!isLogoAnimated && (
-        <motion.div
-          initial={{ scale: 6, opacity: 0, rotate: 360 }}
-          animate={{ scale: 1, opacity: 1, rotate: 0 }}
-          exit={{ scale: 0.2, opacity: 0.5, top: 20, left: 20 }}
-          transition={{ duration: 2, type: "spring" }} // Adjusted for smoother animation
-          onAnimationComplete={() => setIsLogoAnimated(true)}
-          className="fixed inset-0 flex items-center justify-center z-50" // Ensured full coverage and higher z-index
-        >
-          <Image
-            src="/images/logo.png"
-            alt="Logo"
-            width={200} // Adjusted image size
-            height={200}
-            className="rounded-full shadow-md"
-          />
-        </motion.div>
-      )}
+      <AnimatePresence>
+        {!isLogoAnimated && (
+          <motion.div
+            variants={logoVariants}
+            transition={{ duration: 2, type: "spring" }}
+            onAnimationComplete={handleAnimationComplete}
+            className="fixed inset-0 flex items-center justify-center z-50"
+          >
+            <Image
+              src="/images/logo.png"
+              alt="Logo"
+              width={200}
+              height={200}
+              className="rounded-full shadow-lg"
+            />
+          </motion.div>
+        )}
+      </AnimatePresence>
 
-      {isLogoAnimated && (
+      {isContentVisible && (
         <>
           <motion.div
-            initial={{ y: -30, opacity: 0 }}
-            animate={{ y: 0, opacity: 1 }}
+            variants={contentVariants}
             transition={{ delay: 0.5, duration: 0.7 }}
-            className="w-full"
+            className="w-full px-4 py-2"
           >
             <Board />
           </motion.div>
 
           <motion.p
-            initial={{ y: 20, opacity: 0 }}
-            animate={{ y: 0, opacity: 1 }}
+            variants={contentVariants}
             transition={{ delay: 1, duration: 0.7 }}
-            className="text-gray-500 font-medium mt-5 text-sm"
+            className="text-gray-300 font-semibold my-2 text-sm tracking-wide"
           >
             Created by Rahul Ragi
           </motion.p>
